74349a0653cb73b1d25d4792fd2258a2fb1c7623
choptop84 <monecraftwithmike123@gmail.com>
2025-09-25 02:00:30 -0400

    Added color for the prompt

--- editor/AddSamplesPrompt.ts
+++ editor/AddSamplesPrompt.ts
@@ -45,7 +45,7 @@ export class AddSamplesPrompt {
         this._addSampleButton,
         this._addMultipleSamplesButton
     );
-    private readonly _instructionsLink: HTMLAnchorElement = a({ href: "#", style:"color:red;font-weight:bold;"}, "> Click Here for instructions on adding samples <");
+    private readonly _instructionsLink: HTMLAnchorElement = a({ href: "#", style:"color:var(--loop-accent, red); font-weight:bold;"}, "> Click Here for instructions on adding samples <");
     private readonly _description: HTMLDivElement = div(
         div({ style: "margin-bottom: 0.5em; -webkit-user-select: text; -moz-user-select: text; -ms-user-select: text; user-select: text; cursor: text;" },
             "In order to use the old UltraBox samples, you should add ",
